0849b5f04ae773727d74cf1c99c1dcaf2703b62d
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-23 13:54:30 +0700

    Add main navigation host

---
+++ app/src/main/java/com/salt/apps/notes/presentation/navigation/MainNavHost.kt
@@ -0,0 +1,25 @@
+package com.salt.apps.notes.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.salt.apps.notes.presentation.components.ViewPagerConfig
+import com.salt.apps.notes.presentation.navigation.Screen.Main
+import com.salt.apps.notes.presentation.screens.add.navigation.addGraph
+import com.salt.apps.notes.presentation.screens.main.navigation.mainGraph
+
+@Composable
+fun MainNavHost(
+    navController: NavHostController,
+    viewPagerConfig: ViewPagerConfig,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = Main.route, modifier = modifier) {
+        mainGraph(
+            viewPagerConfig = viewPagerConfig
+        )
+        addGraph()
+    }
+}
+
