220ee777be84521a4a4e5b7eadd894497aacf201
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-24 19:23:18 +0700

    Update repository

---
+++ app/src/main/java/com/salt/apps/notes/data/repository/Repository.kt
@@ -0,0 +1,16 @@
+package com.salt.apps.notes.data.repository
+
+import com.salt.apps.notes.data.local.entity.NoteEntity
+import com.salt.apps.notes.data.local.room.dao.NoteDao
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Repository @Inject constructor(private val noteDao: NoteDao) {
+    suspend fun insertNote(noteEntity: NoteEntity) = noteDao.insertNote(noteEntity = noteEntity)
+    suspend fun updateNote(noteEntity: NoteEntity) = noteDao.updateNote(noteEntity = noteEntity)
+    fun getAllNotes(): Flow<List<NoteEntity>> = noteDao.getAllNotes()
+    suspend fun deleteNote(noteEntity: NoteEntity) = noteDao.deleteNote(noteEntity = noteEntity)
+    fun deleteAllNotes() = noteDao.deleteAllNotes()
+}
