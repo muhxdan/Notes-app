70f13133b412b4d7b39b412340ddecfc62a9343e
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-24 19:22:22 +0700

    Update note entity

---
+++ app/src/main/java/com/salt/apps/notes/data/local/entity/NoteEntity.kt
@@ -0,0 +1,13 @@
+package com.salt.apps.notes.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes")
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String?,
+    val description: String?,
+    val date: String,
+)
