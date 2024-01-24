1e8c0dc22176fa8c06c878f87eff944de1bac5ca
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-24 19:24:17 +0700

    Add loading components

---
+++ app/src/main/java/com/salt/apps/notes/presentation/components/Loading.kt
@@ -0,0 +1,26 @@
+package com.salt.apps.notes.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Loading() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .size(20.dp)
+        )
+    }
+}
