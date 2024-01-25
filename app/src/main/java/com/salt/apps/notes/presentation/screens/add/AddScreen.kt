145f76878ffe5cb9cbb991ff9980e8050e6d84a8
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-25 21:02:41 +0700

    Update add note screen

--- app/src/main/java/com/salt/apps/notes/presentation/screens/add/AddScreen.kt
+++ app/src/main/java/com/salt/apps/notes/presentation/screens/add/AddScreen.kt
@@ -1,20 +1,67 @@
 package com.salt.apps.notes.presentation.screens.add
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.salt.apps.notes.presentation.components.TextField
+import com.salt.apps.notes.presentation.components.TopAppBar
 
 @Composable
-fun AddScreen() {
+fun AddScreen(
+    navController: NavHostController,
+    addViewModel: AddViewModel = hiltViewModel(),
+) {
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+    val currentTime by addViewModel.currentTime.collectAsState()
+
     Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Add Screen")
+        TopAppBar(
+            canNavigateBack = navController.previousBackStackEntry != null,
+            navigateUp = {
+                navController.navigateUp()
+            },
+            isActionsActive = title.trim().isNotEmpty() || description.trim().isNotEmpty(),
+            isActionsClick = {
+                addViewModel.insertNote(
+                    title = title,
+                    description = description
+                )
+                navController.navigateUp()
+            }
+        )
+        TextField(title = title, placeHolder = "Title", onValueChange = { title = it })
+        Text(
+            text = currentTime,
+            style = MaterialTheme.typography.bodySmall.copy(
+                color = if (isSystemInDarkTheme()) Color(0xff686868) else Color(0xFF8F8F8F)
+            ),
+            modifier = Modifier.padding(horizontal = 20.dp)
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+        TextField(
+            title = description,
+            placeHolder = "Description",
+            onValueChange = { description = it })
     }
 }
+
