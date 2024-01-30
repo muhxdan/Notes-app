b9027e07aa81b00f1caf78fb467e318a67864e90
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-30 21:01:29 +0700

    Delete some composable components

--- app/src/main/java/com/salt/apps/notes/presentation/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.salt.apps.notes.presentation.navigation
-
-sealed class Screen(
-    val route: String,
-) {
-    data object Main : Screen("main")
-    data object Add : Screen("add")
-}
