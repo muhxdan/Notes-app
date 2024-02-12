f69d0f1446374514d09af24359961e6b6301c454
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-02-12 20:16:55 +0700

    Update main top bar

--- app/src/main/java/com/salt/apps/notes/presentation/screens/main/components/MainTopBar.kt
+++ app/src/main/java/com/salt/apps/notes/presentation/screens/main/components/MainTopBar.kt
@@ -1,8 +1,9 @@
 package com.salt.apps.notes.presentation.screens.main.components
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,15 +15,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.salt.apps.notes.R
@@ -38,80 +36,85 @@ fun MainTopBar(
     onSettingsClicked: () -> Unit,
     mainViewModel: MainViewModel,
 ) {
-
     val systemUiController = rememberSystemUiController()
-    var isSelected by remember { mutableStateOf(false) }
+    val isSelected = mainViewModel.screenState.value.isAppBarOpened
 
-    LaunchedEffect(mainViewModel.screenState.value.selectedNotes) {
-        isSelected = mainViewModel.screenState.value.selectedNotes.isNotEmpty()
-    }
-
-    val backgroundColor by animateColorAsState(
-        if (mainViewModel.screenState.value.selectedNotes.isNotEmpty()) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.background,
-        animationSpec = tween(200), label = "",
-    )
-
+    val backgroundColor =
+        if (isSelected) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.background
     SideEffect {
         systemUiController.setStatusBarColor(backgroundColor)
     }
 
     Column {
-        MediumTopAppBar(
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = backgroundColor,
-            ),
-            title = {
-                Text(
-                    if (isSelected) {
-                        if (mainViewModel.screenState.value.selectedNotes.isEmpty()) {
-                            stringResource(R.string.title_notes)
+        AnimatedVisibility(visible = !mainViewModel.screenState.value.isSearchActive) {
+            MediumTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = backgroundColor,
+                    scrolledContainerColor = backgroundColor
+                ),
+                title = {
+                    Text(
+                        text = if (isSelected) {
+                            if (mainViewModel.screenState.value.selectedNotes.isEmpty()) {
+                                stringResource(R.string.title_notes)
+                            } else {
+                                "${mainViewModel.screenState.value.selectedNotes.size} items selected"
+                            }
                         } else {
-                            "${mainViewModel.screenState.value.selectedNotes.size} items selected"
+                            if (currentScreen == MainScreen.Note.route) stringResource(R.string.title_notes) else stringResource(
+                                R.string.title_tasks
+                            )
+                        }
+                    )
+                },
+                actions = {
+                    if (isSelected) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            CustomIconButton(
+                                onClick = mainViewModel::deleteSelectedNotes,
+                                icon = R.drawable.ic_trash
+                            )
+                            CustomIconButton(
+                                onClick = mainViewModel::toggleAllNotes,
+                                icon = R.drawable.ic_check_all
+                            )
                         }
                     } else {
-                        if (currentScreen == MainScreen.Note.route) stringResource(R.string.title_notes) else stringResource(
-                            R.string.title_tasks
+                        CustomIconButton(onClick = onSettingsClicked, icon = R.drawable.ic_settings)
+                    }
+                },
+                scrollBehavior = scrollOnlyInTopBehavior,
+                navigationIcon = {
+                    if (isSelected) {
+                        CustomIconButton(
+                            onClick = mainViewModel::clearSelectedNotes,
+                            icon = R.drawable.ic_close,
+                            size = 16.dp
                         )
                     }
-                )
-            },
-            actions = {
-                if (isSelected) {
-                    IconButton(onClick = mainViewModel::selectAllNotes) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_check_all),
-                            contentDescription = null
-                        )
-                    }
-                } else {
-                    IconButton(onClick = onSettingsClicked) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_settings),
-                            contentDescription = null
-                        )
-                    }
-                }
-            }, scrollBehavior = scrollOnlyInTopBehavior,
-            navigationIcon = {
-                if (isSelected) {
-                    IconButton(
-                        onClick = mainViewModel::clearSelectedNote,
-                    ) {
-                        Icon(
-                            painterResource(id = R.drawable.ic_close),
-                            contentDescription = null,
-                            modifier = Modifier.size(17.dp)
-                        )
-                    }
-                }
-            }
-        )
+                },
+            )
+        }
         CenterAlignedTopAppBar(
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.background,
-            ),
-            title = { Text(text = "Search bar") },
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.background),
+            title = {
+                CustomSearchBar(mainViewModel)
+            },
             scrollBehavior = scrollBehavior,
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
+    }
+}
+
+@Composable
+fun CustomIconButton(onClick: () -> Unit, icon: Int, size: Dp = 18.dp) {
+    IconButton(onClick = onClick) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier.size(size)
         )
     }
 }
