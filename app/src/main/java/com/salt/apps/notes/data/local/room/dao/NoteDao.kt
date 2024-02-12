4fcdcc298508539c036464defb60fa59c1c5d71a
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-02-12 20:15:20 +0700

    Update dao delete

--- app/src/main/java/com/salt/apps/notes/data/local/room/dao/NoteDao.kt
+++ app/src/main/java/com/salt/apps/notes/data/local/room/dao/NoteDao.kt
@@ -1,7 +1,6 @@
 package com.salt.apps.notes.data.local.room.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -16,8 +15,8 @@ interface NoteDao {
     @Query("SELECT * FROM notes")
     fun getAllNotes(): Flow<List<NoteEntity>>
 
-    @Delete
-    suspend fun deleteNote(noteEntity: NoteEntity)
+    @Query("DELETE FROM notes WHERE id = :noteId")
+    suspend fun deleteNote(noteId: Int)
 
     @Query("DELETE FROM notes")
     fun deleteAllNotes()
